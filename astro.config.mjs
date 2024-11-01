ff29a6f289fbfccde1b75f5114037cd938add33a
Matt Kane <m@mk.gg>
2024-11-01 17:54:51 +0000

    Icon stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: `https://mk.gg`,
+
   experimental: {
     contentLayer: true,
   },
+
+  integrations: [icon()],
 });
